65a02438760041a7e0d701c50afb5d427fae24f0
Biniam777 <153329020+Biniam777@users.noreply.github.com>
2025-05-12 23:01:30 +0300

    Add files via upload

---
+++ backend/middlewares/uploadImage.js
@@ -0,0 +1,20 @@
+// middleware/uploadImage.js
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "Images"); // save directly to Images/
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext);
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: { fileSize: 5000000 }, // 5MB limit
+});
+
+module.exports = upload;
